8ca7c600900f8f2d6dc27fe1c23b454a6bdd94e3
LenOfAl <89646341+LenOfAl@users.noreply.github.com>
2022-07-22 10:18:36 +0530

    new

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-
-def create_app():
-    app = Flask("web")
-    app.register_blueprint
-    from . import web
-    app.register_blueprint(web.bp)
-    return app
--- web.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-bp = Blueprint("web", "web", url_prefix="/")
-
-
-@bp.route("/")
-def index():
-    return render_template("website.html")
