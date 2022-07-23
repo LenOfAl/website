ed99cde49fb3ca45c059b758622738f7b5b47b2d
LenOfAl <89646341+LenOfAl@users.noreply.github.com>
2022-07-23 16:25:37 +0530

    image and sticky scrolling

--- test.js
+++ test.js
@@ -1,25 +1,12 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
+window.onscroll = function () {
+  myFunction();
+};
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
 function myFunction() {
-  var classname = document.getElementById("myDropdown").classList;
-
-  if (classname.contains("show2")) {
-    classname.remove("show2");
+  if (window.scrollY >= sticky) {
+    navbar.classList.add("sticky");
   } else {
-    classname.add("show2");
+    navbar.classList.remove("sticky");
   }
 }
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function (event) {
-  if (!event.target.matches(".dropbtnmain")) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains("show")) {
-        openDropdown.classList.remove("show");
-      }
-    }
-  }
-};
