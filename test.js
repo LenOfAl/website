0150a26bfbdbd53357b9dded255569f4a06da2a9
LenOfAl <89646341+LenOfAl@users.noreply.github.com>
2022-07-24 19:57:30 +0530

    adding support for moblie

--- test.js
+++ test.js
@@ -36,15 +36,4 @@ function getAdjective() {
   document.getElementById("me").innerText = item;
 }
 
-window.onscroll = function () {
-  myFunction();
-};
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-function myFunction() {
-  if (window.scrollY >= sticky) {
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+
