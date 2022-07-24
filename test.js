0e19ff344775a553ebfc133195214ecd914c1b8f
LenOfAl <89646341+LenOfAl@users.noreply.github.com>
2022-07-24 12:59:53 +0530

    added colums

--- test.js
+++ test.js
@@ -10,3 +10,38 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+let items = [
+  Ambitious,
+  Amiable,
+  Bold,
+  Confident,
+  Conscientious,
+  Creative,
+  Decisive,
+  Dependable,
+  Diligent,
+  Enthusiastic,
+  Dependable,
+  Extrovert,
+  Flexible,
+  Friendly,
+  Honest,
+  Imaginative,
+  Influencing,
+  Inspirational,
+  Leader,
+  Loyal,
+  Organized,
+  Perceptive,
+  Personable,
+  Reliable,
+  Resourceful,
+  Versatile,
+  Visionary,
+];
+
+function getAdjective() {
+  var item = items[Math.floor(Math.random() * items.length)];
+  return item;
+}
+document.getElementById("me").innerHTML = getAdjective();
