b9d7a536aec42b6653f019708d65b20310e05eb2
LenOfAl <89646341+LenOfAl@users.noreply.github.com>
2022-07-24 16:01:58 +0530

    newdp

--- test.js
+++ test.js
@@ -1,3 +1,41 @@
+let items = [
+  "Ambitious",
+  "Amiable",
+  "Bold",
+  "Confident",
+  "Conscientious",
+  "Creative",
+  "Decisive",
+  "Dependable",
+  "Diligent",
+  "Enthusiastic",
+  "Dependable",
+  "Extrovert",
+  "Flexible",
+  "Friendly",
+  "Honest",
+  "Imaginative",
+  "Influencing",
+  "Inspirational",
+  "Leader",
+  "Loyal",
+  "Organized",
+  "Perceptive",
+  "Personable",
+  "Reliable",
+  "Resourceful",
+  "Versatile",
+  "Visionary",
+];
+window.onload = () => {
+  getAdjective();
+};
+
+function getAdjective() {
+  var item = items[Math.floor(Math.random() * items.length)];
+  document.getElementById("me").innerText = item;
+}
+
 window.onscroll = function () {
   myFunction();
 };
@@ -10,38 +48,3 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
-let items = [
-  Ambitious,
-  Amiable,
-  Bold,
-  Confident,
-  Conscientious,
-  Creative,
-  Decisive,
-  Dependable,
-  Diligent,
-  Enthusiastic,
-  Dependable,
-  Extrovert,
-  Flexible,
-  Friendly,
-  Honest,
-  Imaginative,
-  Influencing,
-  Inspirational,
-  Leader,
-  Loyal,
-  Organized,
-  Perceptive,
-  Personable,
-  Reliable,
-  Resourceful,
-  Versatile,
-  Visionary,
-];
-
-function getAdjective() {
-  var item = items[Math.floor(Math.random() * items.length)];
-  return item;
-}
-document.getElementById("me").innerHTML = getAdjective();
